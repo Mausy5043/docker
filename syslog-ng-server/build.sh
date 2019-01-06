ee0784bd0b10d9c363ed071c748ddfdd2f1a49a3
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 14:25:25 +0100

    cleanup

--- syslog-ng-server/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# build a local image
-docker build -t mausy5043/syslog-ng-server:latest .
--- syslog-ng-server/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-options="$@"
-
-docker run --rm -it mausy5043/syslog-ng-server:latest "${options}"
